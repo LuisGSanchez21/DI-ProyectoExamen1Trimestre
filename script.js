ead3cb9033924e3eae7c3ced6a36bf57ae44ed3e
LuisGS <154208899+LuisGSanchez21@users.noreply.github.com>
2024-11-18 14:18:04 +0100

    clase del 18/11

--- script.js
+++ script.js
@@ -1,48 +1,100 @@
-class NewsViewer extends HTMLElement {
+class ProductViewer extends HTMLElement {
     constructor() {
       super();
     }
   
     connectedCallback() {
-      this.loadArticles();
+      this.loadproducts();
     }
   
-    async loadArticles() {
+    async loadproducts() {
       try {
         const response = await fetch('https://products-foniuhqsba-uc.a.run.app/Cameras');
         if (!response.ok) {
-          throw new Error('Error al obtener los artículos');
+          throw new Error('Error al obtener los productos');
         }
-        const articles = await response.json();
-        this.renderArticles(articles);
+        const products = await response.json();
+        this.renderproducts(products);
       } catch (error) {
         console.error('Error:', error);
         this.innerHTML = `<p>Error al cargar los artículos. Inténtelo nuevamente más tarde.</p>`;
       }
     }
   
-    renderArticles(articles) {
-      const template = document.getElementById('article-template');
+    renderproducts(products) {
+      const template = document.getElementById('product-template');
       
       // Limpiar contenido existente
       this.innerHTML = '';
   
-      articles.forEach(article => {
+      products.forEach(product => {
         // Clonar el contenido de la plantilla
-        const articleContent = document.importNode(template.content, true);
+        const productContent = document.importNode(template.content, true);
         
-        // Rellenar la plantilla con los datos del artículo
-        articleContent.querySelector('.title').textContent = article.headline;
-        articleContent.querySelector('.short_desciption').textContent = article.author;
-        articleContent.querySelector('.description').innerHTML = article.body;
+        productContent.querySelector('.title').textContent = product.title;
+        productContent.querySelector('.short_description').textContent = product.short_description;
+ //       productContent.querySelector('.description').textContent = product.description;
+ //       productContent.querySelector('.date').textContent = product.date;
+  //      productContent.querySelector('.category').textContent = product.category;
+ //       productContent.querySelector('.rating').textContent = product.rating;
+       productContent.querySelector('.price').textContent = product.price;
+        
+        // Asignar la imagen al atributo src del <img>
+        productContent.querySelector('.img').src = product.image;
         
         // Añadir el artículo al componente
-        this.appendChild(articleContent);
+        this.appendChild(productContent);
       });
     }
   }
   
   // Definir el elemento personalizado
-  customElements.define('news-viewer', NewsViewer);
+  customElements.define('product-viewer', ProductViewer);
 
+  class RelativeTime extends HTMLElement {
+    constructor() {
+      super();
+    }
+    connectedCallback() {
+      this.render();
+      setInterval(() => this.render(), 1000);
+    }
+    static get observedAttributes() {
+      return ['time'];
+    }
+    attributeChangedCallback(name, oldValue, newValue) {
+      this.render();
+    }
+    render() {
+      const timeValue = this.getAttribute('time')
+      const time = timeValue ? new Date(Number(timeValue)).getTime() : Date.now();
+      const now = Date.now();
+  
+      const diff = now - time;
+      const seconds = Math.floor(diff / 1000) || 1;
+      const minutes = Math.floor(diff / (1000 * 60));
+      const hours = Math.floor(diff / (1000 * 60 * 60));
+      const days = Math.floor(hours / 24);
+      const months = Math.floor(days / 30);
+      const years = Math.floor(months / 12);
+  
+      let aux = '...';
+      if (months >= 12) {
+        aux = `Hace ${years} año${years > 1 ? 's' : ''}`
+      } else if (days > 30 && months >= 1) {
+        aux = `Hace ${months} mes${months > 1 ? 'es' : ''}`
+      } else if (days >= 1) {
+        aux = `Hace ${days} día${days > 1 ? 's' : ''}`
+      } else if (hours >= 1) {
+        aux = `Hace ${hours} hora${hours > 1 ? 's' : ''}`
+      } else if (minutes >= 1) {
+        aux = `Hace ${minutes} minuto${minutes > 1 ? 's' : ''}`
+      } else if (seconds >= 1) {
+        aux = `Hace ${seconds} segundo${seconds > 1 ? 's' : ''}`
+      }
+  
+      this.textContent = aux;
+    } 
+  }
+  customElements.define('relative-time', RelativeTime);
   
